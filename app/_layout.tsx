ebe85ec203ebf7d111137d3fd1e204fddf8aaa6e
zayin <zayinm300@gmail.com>
2025-12-12 21:33:50 +0000

    Add fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,33 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-    return <Stack></Stack>;
+    const [loaded, error] = useFonts({
+        "Bitter-Bold": require("../assets/fonts/Bitter-Bold.ttf"),
+        "Bitter-ExtraBold": require("../assets/fonts/Bitter-ExtraBold.ttf"),
+        "Bitter-Light": require("../assets/fonts/Bitter-Light.ttf"),
+        "Bitter-Medium": require("../assets/fonts/Bitter-Medium.ttf"),
+        "Bitter-Regular": require("../assets/fonts/Bitter-Regular.ttf"),
+    });
+
+    useEffect(() => {
+        if (loaded || error) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded, error]);
+
+    if (!loaded && !error) {
+        return null;
+    }
+    return (
+        <Stack>
+            <Stack.Screen name="index" options={{ headerShown: false }} />
+            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="+not-found" />
+        </Stack>
+    );
 }
