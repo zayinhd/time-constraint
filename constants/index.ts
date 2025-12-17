72f9c01a441b60a48bb42fd63020c6a89c8b19af
zayin <zayinm300@gmail.com>
2025-12-17 21:35:49 +0000

    made some few changes

---
+++ constants/index.ts
@@ -0,0 +1,19 @@
+import analytics from "@/assets/icons/analytics.png";
+import backArrow from "@/assets/icons/back-arrow.png";
+import close from "@/assets/icons/close.png";
+import email from "@/assets/icons/email.png";
+import home from "@/assets/icons/home.png";
+import lock from "@/assets/icons/lock.png";
+import person from "@/assets/icons/person.png";
+import profile from "@/assets/icons/profile.png";
+
+export const icons = {
+    backArrow,
+    close,
+    email,
+    home,
+    analytics,
+    lock,
+    person,
+    profile,
+};
